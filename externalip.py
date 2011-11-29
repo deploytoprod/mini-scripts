ade295507c5780333d3762caa204a82a66843472
Rafael Lopes <dev@rafalopes.com.br>
2011-11-29 01:28:32 -0200

    aplicando merge

--- externalip.py
+++ externalip.py
@@ -1,18 +1,18 @@
 '''
-This script just finds your external IP address
-Bugs and tickets, please email me: dev@rafalopes.com.br :)
+Finds your external IP address
 '''
 
 import urllib
 import re
 
-url = urllib.URLopener()
-resp = url.open('http://myip.dk')
-html = resp.read()
+def get_ip():
+    url = urllib.URLopener()
+    resp = url.open('http://myip.dk')
+    html = resp.read()
+    group=re.compile(u'(?P<ip>\d+\.\d+\.\d+\.\d+)').search(html).groupdict()
+    return group['ip']
 
-start = html.find('ha4') #watch this line if the script stops working
-end = start + 50 #a bunch of chars to play along :)
-
-trim=re.compile(u'(?P<ip>\d+\.\d+\.\d+\.\d+)').search(html[start:end]).groupdict()
-
-print trim['ip']
+=======
+if __name__ == '__main__':
+    print get_ip()
+>>>>>>> 7cd49b48087f54d6931f03595821c6aa45d67ecb
