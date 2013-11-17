e407b761d0b402702e61b9a4048ff87981381779
Rafael Lopes <dev@rafalopes.com.br>
2013-11-17 15:20:01 -0200

    Now it's ready

--- fcp_bkp/fcp_bkp.sh
+++ fcp_bkp/fcp_bkp.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh -
 #About: this script copies my FCP folders to the external HDD.
 set -o pipefail
 
@@ -6,21 +6,31 @@ SRC="/Users/rafael/Movies"
 DST="/Volumes/Box"
 
 function copyFiles(){
-    rsync -av $SRC/Final\ Cut\ Events/* $DST/Final\ Cut\ Events/
-    rsync -av $SRC/Final\ Cut\ Projects/* $DST/Final\ Cut\ Projects/
+    rsync -av $SRC/Final\ Cut\ Events/ $DST/Final\ Cut\ Events/
+    testError $? "Events"
+    rsync -av $SRC/Final\ Cut\ Projects/ $DST/Final\ Cut\ Projects/
+    testError $? "Projects"
 }
 function showNotification(){
     ./terminal-notifier -title "$1" -message "$2"
 }
+function testError(){
+    if test $1 != 0; then
+        showNotification "fcp_bkp - Error" "Something went wrong ($2), please run the script again."
+        exit 2
+    fi
+}
 function checkRunning(){
-    if [ `pgrep $*` != 1 ];then
-        showNotification "fcp_bkp - Error" "FCP is running, please exit."
-        return 1;
+    if pgrep $* > /dev/null
+    then
+        showNotification "fcp_bkp - Error" "$* is running, please exit."
+        exit 1
     fi
 }
 function main(){
     checkRunning "Final Cut"
     copyFiles
+    showNotification "fcp_bkp - Success" "Sync success, please check on Finder."
 }
 
 main
